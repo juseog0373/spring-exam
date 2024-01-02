ea01692be18fcf11ca26b163bcf7ae3946edf175
jskim <juseog0373@naver.com>
2024-01-02 19:06:41 +0900

    20240102 할인정책개발, 새로운할인정책적용 DI(AppConfig) 설정 commit

---
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,25 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService {
+
+    private final MemberRepository memberRepository; // DIP 위반 방지를 위해 인터페이스에만 의존하도록 선언
+    private final DiscountPolicy discountPolicy; // DIP 위반 방지를 위해 인터페이스에만 의존하도록 선언
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, itemPrice);
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
